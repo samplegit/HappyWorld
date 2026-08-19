69dce7a78e39dd95d6a37f0a48189e0f2440e91e
KS <dagda@hanafos.com>
2026-08-19 13:33:17 +0900

    add Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+	public static void main(String[] args){
+		System.out.println("Hello Git World");
+		System.out.println("Second Universe");
+		System.out.println("3th 작업을 추가함");
+		System.out.println("Second branch 에서 작업함");
+		/*   세컨드 브랜치에서 추가한 내용 */ 
+	}
+}
